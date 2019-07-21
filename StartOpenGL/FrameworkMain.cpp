9a2b4356a8a440d1cb1947618ec701e3ed755071
HyeonMyeongJang <gkagm2@gmail.com>
2019-07-21 23:11:18 +0900

    자동 화면 변환

--- StartOpenGL/FrameworkMain.cpp
+++ StartOpenGL/FrameworkMain.cpp
@@ -9,27 +9,41 @@
 #include <iostream>
 #elif DEFINE_STDIO_H
 #include <stdio.h>
-#endif // DEFINE_IOSTREAM
+#endif // DEFINE_IcOSTREAM
 
 #include <stdlib.h>
 #include "GL/glut.h"
 
 namespace SagacityEngine {
 	class GLController {
+	private:
+		int width;
+		int height;
 	public:
 		GLfloat x, y, z;
 		GLController(const GLfloat x = 0, const GLfloat y = 0, const GLfloat z = 0) {
 			this->x = x;
 			this->y = y;
 			this->z = z;
+
+			width = 0;
+			height = 0;
 		}
+
 
 
 		void SetPosition(const GLfloat x, const GLfloat y, const GLfloat z);
 		void Debug_Log();
 		void GLInit();
+		void ShowScreen();
+		void SetWindowSize(int width, int height);
+		int GetWidth();
+		int GetHeight();
+		void MakeCube();
+		void MakeRect();
 	};
 
+	//// FUNCTION
 	void GLController::Debug_Log() {
 #ifdef DEFINE_STDIO_H
 		printf("error!");
@@ -43,6 +57,33 @@ namespace SagacityEngine {
 	}
 	void GLController::GLInit() {
 
+	}
+
+	void GLController::ShowScreen() {
+
+	}
+	void GLController::SetWindowSize(int width, int height) {
+		this->width = width;
+		this->height = height;
+	}
+
+	int GLController::GetWidth() {
+		return width;
+	}
+	int GLController::GetHeight() {
+		return height;
+	}
+
+	void GLController::MakeCube() {
+		glutSolidCube(5.0);
+	}
+	void GLController::MakeRect() {
+		glBegin(GL_POLYGON);
+		glVertex3f(-.5, -.5, 0);
+		glVertex3f(.5, -.5, 0);
+		glVertex3f(.5, .5, 0);
+		glVertex3f(-.5, .5, 0);
+		glEnd();
 	}
 	
 }
@@ -61,20 +102,14 @@ GLController controller;
 
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POLYGON);
-	  
-		glVertex3f(controller.x, -.3, .0);
-		glVertex3f(.3, -.3, .0);
-		glVertex3f(.3, .3, .0);
-		glVertex3f(-.3, .3, .0);
+	//
+	glViewport(0, 0, controller.GetWidth(), controller.GetHeight());
+	printf("widht :  %d, height : %d\n", controller.GetWidth(), controller.GetHeight());
+	//
 
-		glVertex3d(3, 5, 0);
-		glVertex3d(5, 5, 0);
-		glVertex3d(5, 3, 0);
-	
-	
-	glEnd();
-	
+	controller.MakeRect();
+
+	printf("Display 龋免\n");
 	glFlush();
 }
 
@@ -88,24 +123,33 @@ void MyMouse(GLint button, GLint state, GLint x, GLint y) {
 
 
 void MyIdle() {
-
 }
 
 void MyReshape(int width, int height) {
-
+	printf("width : %d, height : %d\n", width, height);
+	controller.SetWindowSize(width, height);
 }
 
 int main() {
 
 
 	controller.Debug_Log();
+	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Game"); // 货肺款 扩档快 积己
 	controller.GLInit();
+	glClearColor(0.5, 0.5, 0.5, 1.0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	//
 	glutDisplayFunc(MyDisplay);
 	glutKeyboardFunc(MyKeyboard);
 	glutMouseFunc(MyMouse);
 	glutIdleFunc(MyIdle);
 	glutReshapeFunc(MyReshape);
+	//
+
 	glutMainLoop();
 	
 	return 0;
